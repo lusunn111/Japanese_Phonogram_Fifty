33fa0080fa15c97c800908f63d690ceae504f4bc
Mlxxs <mzh9966@foxmail.com>
2020-04-09 21:25:37 +0800

    111

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<!-- saved from url=(0014)about:internet -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>五十音图💪</title>
+<link rel="icon" type="image/ico" href="https://cdn.jsdelivr.net/gh/mlxxs/photos/backg/anonymous/favicon.ico">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/mlxxs/photos/wsy/mdui.min.css">
+<script src="https://libs.baidu.com/jquery/2.1.4/jquery.min.js"></script>
+<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
+<link href="https://cdn.jsdelivr.net/gh/mlxxs/photos@master/wsy/vie.css" rel="stylesheet" type="text/css"> 
+
+
+</head>
+<body class="mdui-loaded">
+
+  <div class="zhuti">
+  <div class="body"></div>
+  <div class="zhezhao"></div>
+<div class="card">
+        <br>
+        <br> 
+        <center>
+        <div class="mdui-chip" style="width:200px;background-color: dimgray">
+        <span class="mdui-chip-title" style="position:relative">五十音图学习</span>
+        </div>
+     </center>
+              <br>
+<div class="title">
+	<ul>
+		<li>网站作者：<a href="https://lxxs.xyz">路讯先生</a></li>
+		<li>样式取自：<a href="https://serab.cn/">Yueer</a> </li>
+		<li>图片来源：<a href="https://www.bilibili.com/video/BV1Ds411273r?p=1">明王道</a></li>
+		</ul>
+		</div>
+	<br>
+<div>
+<!--积累一下哈。。-->	
+<?php
+	function imgs()
+	{
+    	$url = 'https://cdn.jsdelivr.net/gh/自己的路径'.(string)rand(自己的编号).'.jpg';
+    	echo $url;
+	}
+?>
+
+<div><img src=<?php imgs() ?> class="mdui-img-fluid mdui-shadow-5" style="width:100%;"></div>	
+<!--积累结束-->
+<center>
+Site Powered by <a target="_self" href="https://lxxs.xyz/app/wsy">点击更换</a> 
+</center>
+
+</div>
+</div>
+<br><br><br><br>
+<br><br><br><br>
+<br><br><br><br>
+<span style="font-size:15px;">
+<center>加油，快快背过他。(ง •_•)ง💪 <p></p>
+Site Powered by <a target="_blank" href="https://lxxs.xyz">路讯先生</a> | HOST Powered by <a target="_blank" href="https://www.yunlab.me/">云喇叭</a>
+</center>
+</span>
+<br>
+<span style="font-size:15px;">
+<center>刷新即可更换卡片，如有大佬帮助实现更多功能感激不尽</center>
+<br><center>目前更新到た行</center>
+</span>
+<br><br><br><br>
+<br><br><br><br>
+</div>
+</body>
+</html>
